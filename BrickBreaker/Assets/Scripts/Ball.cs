0ab1557a97886d135ae62b6e3508a5157eec7ec6
Nitish Kumar <nitishk730@gmail.com>
2023-03-21 17:50:13 +0530

    New Additions(Ball.cs & Paddle.cs)
    Ball movements added and changes made foe paddle

--- BrickBreaker/Assets/Scripts/Ball.cs
+++ BrickBreaker/Assets/Scripts/Ball.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public new Rigidbody2D rigidbody { get; private set; }
+
+    public float speed = 100f;
+
+    private void Awake()
     {
-        
+        this.rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Invoke(nameof(DelayAfterStart), 1f);
+    }
+
+    private void DelayAfterStart()
+    {
+        Vector2 force = Vector2.zero;
+        force.x = Random.Range(-1f, 1f);
+        force.y = -1f;
+
+        this.rigidbody.AddForce(force.normalized * this.speed);
     }
 }
